c58d8a82887dd5555311072ae08071a6ba8a2c0d
IanGrubb <hi@iangrubb.com>
2019-10-26 18:45:14 -0400

    validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,20 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+
+    const leftCheck =
+        !node.left ||
+        (!min && node.left.data < node.data && validate(node.left, min, node.data)) ||
+        (min < node.left.data && node.left.data < node.data && validate(node.left, min, node.data))
+
+    const rightCheck = 
+        !node.right ||
+        (!max && node.right.data > node.data && validate(node.right, node.data, max)) ||
+        (max > node.right.data && node.right.data > node.data && validate(node.right, node.data, max))
+    
+    return leftCheck && rightCheck
+
+}
 
 module.exports = validate;
